d6e780ef7f1b0390be9f04bd005dfa8d9cfd76f8
Aleksandr <Aleksandr>
2024-10-10 18:33:22 +0300

    add: car object

---
+++ car_object.ts
@@ -0,0 +1,10 @@
+const car = {
+    salon: 'тёмный',
+    securitySystems: 'ABS',
+    pillows: 'В полной комплектации',
+    exterior: 'Легкоплавкие диски',
+    heating: true
+}
+
+car.salon = 'светлый'
+console.log(car)
